4637342c700a38f524542aa75f198cfd840f52e9
freek <freek@spatie.be>
2020-03-24 10:06:22 +0100

    wip

--- src/StubPublishCommand.php
+++ src/StubPublishCommand.php
@@ -2,12 +2,36 @@
 
 namespace Spatie\Stubs;
 
-use Illuminate\Foundation\Console\StubPublishCommand as IlluminateStubPublishCommand;
+use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Finder\SplFileInfo;
 
-class StubPublishCommand extends IlluminateStubPublishCommand
+class StubPublishCommand extends Command
 {
-    protected $signature = 'stub:publish';
+    protected $signature = 'spatie-stub:publish';
 
     protected $description = 'Publish all opinionated stubs that are available for customization';
 
+    use ConfirmableTrait;
+
+    public function handle()
+    {
+        if (! is_dir($stubsPath = $this->laravel->basePath('stubs'))) {
+            (new Filesystem)->makeDirectory($stubsPath);
+        }
+
+        collect(File::files(__DIR__ . '/../stubs'))->each(function(SplFileInfo $file) use ($stubsPath) {
+            $sourcePath = $file->getPathname();
+
+            $targetPath = $stubsPath . "/{$file->getFilename()}";
+
+            if (! file_exists($targetPath) || $this->option('force')) {
+                file_put_contents($targetPath, file_get_contents($sourcePath));
+            }
+        });
+
+        $this->comment('All done!');
+    }
 }
--- tests/StubPublishCommandTest.php
+++ tests/StubPublishCommandTest.php
@@ -2,11 +2,23 @@
 
 namespace Spatie\Stubs\Tests;
 
+use Illuminate\Support\Facades\File;
+
 class StubPublishCommandTest extends TestCase
 {
     /** @test */
-    public function it_tests()
+    public function it_can_publish_stubs()
     {
-        $this->assertTrue(true);
+        $targetStubsPath = $this->app->basePath('stubs');
+
+        File::deleteDirectory($targetStubsPath);
+
+        $this->artisan('spatie-stub:publish')->assertExitCode(0);
+
+        $stubPath = __DIR__ . '/../stubs/migration.stub';
+
+        $publishedStubPath = $targetStubsPath . '/migration.stub';
+
+        $this->assertFileEquals($stubPath, $publishedStubPath);
     }
 }
