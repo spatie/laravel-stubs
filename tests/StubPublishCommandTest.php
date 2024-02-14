17c6217c2559e93dcd6d2736778a6d0fba4e9d20
Mo Khosh <mskhoshnazar@gmail.com>
2024-02-14 17:12:01 +0330

    tdd it only publishes new stubs for laravel 11+ applications

--- tests/StubPublishCommandTest.php
+++ tests/StubPublishCommandTest.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
 
+use function PHPUnit\Framework\assertFileDoesNotExist;
 use function PHPUnit\Framework\assertFileEquals;
+use function PHPUnit\Framework\assertFileExists;
 
 it('can publish stubs', function () {
     $targetStubsPath = $this->app->basePath('stubs');
@@ -16,4 +19,28 @@ it('can publish stubs', function () {
     $publishedStubPath = $targetStubsPath . '/migration.stub';
 
     assertFileEquals($stubPath, $publishedStubPath);
+});
+
+it('can publish laravel 11 stubs', function () {
+    App::shouldReceive('version')->andReturn('11');
+
+    $targetStubsPath = $this->app->basePath('stubs');
+
+    File::deleteDirectory($targetStubsPath);
+
+    $this->artisan('spatie-stub:publish')->assertExitCode(0);
+
+    assertFileExists($targetStubsPath . '/enum.stub');
+});
+
+it('will not publish laravel 11 stubs for laravel 10 and lower', function () {
+    App::shouldReceive('version')->andReturn('10');
+
+    $targetStubsPath = $this->app->basePath('stubs');
+
+    File::deleteDirectory($targetStubsPath);
+
+    $this->artisan('spatie-stub:publish')->assertExitCode(0);
+
+    assertFileDoesNotExist($targetStubsPath . '/enum.stub');
 });
